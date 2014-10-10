bbcd7206f39a7aef4523802022e3ff7332773f78
Christoph Gockel <github@christophgockel.de>
2014-10-10 11:33:00 +0100

    Extract methods for readability

--- node.rb
+++ node.rb
@@ -24,14 +24,24 @@ module Node
 
   module VisitableContainer
     def accept(visitor)
-      class_name = self.class.to_s.split('::').last.downcase
-      enter_method_name = "visit_enter_#{class_name}".to_sym
-      leave_method_name = "visit_leave_#{class_name}".to_sym
-
       if visitor.send enter_method_name, self
         self.children.each { |child| child.accept(visitor) }
       end
       visitor.send leave_method_name, self
+    end
+
+    private
+
+    def class_name
+      self.class.to_s.split('::').last.downcase
+    end
+
+    def enter_method_name
+      "visit_enter_#{class_name}".to_sym
+    end
+
+    def leave_method_name
+      "visit_leave_#{class_name}".to_sym
     end
   end
 
