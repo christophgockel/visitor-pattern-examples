511a17d2245ea10e385bdfa41a1e1fe76a78366a
Christoph Gockel <github@christophgockel.de>
2014-10-10 01:52:05 +0100

    Add typed visitor example

---
+++ example_typed_visitor.rb
@@ -0,0 +1,58 @@
+require_relative "node"
+
+html = Node::Html.new
+  head = Node::Head.new
+    head.add(Node::Title.new("The Title"))
+  body = Node::Body.new
+    body.add(Node::H1.new("Some Headline"))
+    body.add(Node::P.new("More text..."))
+
+html.add(head)
+html.add(body)
+
+
+class TypedVisitor
+  def visit(node)
+    node.accept(self)
+  end
+
+  def visit_html(node)
+    puts "html"
+
+    node.children.each do |child|
+      child.accept(self)
+    end
+  end
+
+  def visit_head(node)
+    puts "head"
+
+    node.children.each do |child|
+      child.accept(self)
+    end
+  end
+
+  def visit_title(node)
+    puts "title - #{node.content}"
+  end
+
+  def visit_body(node)
+    puts "body"
+
+    node.children.each do |child|
+      child.accept(self)
+    end
+  end
+
+  def visit_h1(node)
+    puts "h1 - #{node.content}"
+  end
+
+  def visit_p(node)
+    puts "p - #{node.content}"
+  end
+end
+
+visitor = TypedVisitor.new
+
+visitor.visit(html)
